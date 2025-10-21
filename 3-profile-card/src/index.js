c1559ef2fc3ca37d9f01167e995d05e6e5986f89
sina <cyrus2nd.tg@gmail.com>
2025-10-21 16:09:27 -0400

    Add picture and text to profile card

--- 3-profile-card/src/index.js
+++ 3-profile-card/src/index.js
@@ -1,3 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+const App = function () {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+const Card = function () {
+  return (
+    <div className="frame">
+      <Pic />
+      <div className="content">
+        <Title />
+        <Description />
+        {/*<Tags /> */}
+      </div>
+    </div>
+  );
+};
+
+const Pic = function () {
+  return (
+    <div className="profPic">
+      <img src="my_photo2.jpg" alt="Sina"></img>
+    </div>
+  );
+};
+
+const Title = function () {
+  return (
+    <div>
+      <h2>Sina Rahimi</h2>
+    </div>
+  );
+};
+
+const Description = function () {
+  return (
+    <div>
+      <p>
+        Full-stack developer and Cs student. I'm always bussy creating usefull
+        Apps and when I'm not doing that, I'm probably playing video games. 🗿
+      </p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
