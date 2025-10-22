1444ba7ca7c327bcae76a16572854d3e1cb90985
sina <cyrus2nd.tg@gmail.com>
2025-10-21 21:53:13 -0400

    Add closing hour condition and out-of-order menu condition

--- 2-pizza-menu/src/index.js
+++ 2-pizza-menu/src/index.js
@@ -66,14 +66,19 @@ function Header() {
 }
 
 function Menu() {
+  const pizaNum = pizzaData.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => {
-          return <Pizza pizzaObject={pizza} key={pizza.name} />;
-        })}
-      </ul>
+      {pizaNum > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => {
+            return <Pizza pizzaObject={pizza} key={pizza.name} />;
+          })}
+        </ul>
+      ) : (
+        <p>We're still working on our menue. Please come back later 🤓</p>
+      )}
     </main>
   );
 }
@@ -100,11 +105,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come vist us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00 🙏
+        </p>
       )}
     </footer>
   );
