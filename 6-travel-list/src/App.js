06f9b3d7265f04a71a6613ebd392951ed33be70b
sina <cyrus2nd.tg@gmail.com>
2025-10-27 19:13:52 -0400

    Add the very basic structure of components statically

--- 6-travel-list/src/App.js
+++ 6-travel-list/src/App.js
@@ -1,5 +1,31 @@
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <States />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴Far Away 👜</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="List"></div>;
+}
+function States() {
+  return (
+    <footer>
+      You have X items on your list, and you already packed X (X%)
+    </footer>
+  );
+}
