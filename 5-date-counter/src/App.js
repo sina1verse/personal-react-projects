3bf2dcbd83f2ccc195cf017ea520fb9134354dba
sina <cyrus2nd.tg@gmail.com>
2025-10-27 00:15:59 -0400

    Add static step, count and dateString elements

--- 5-date-counter/src/App.js
+++ 5-date-counter/src/App.js
@@ -1,3 +1,38 @@
-function App() {}
+export default function App() {
+  return (
+    <>
+      <Step />
+      <Count />
+      <DateString />
+    </>
+  );
+}
 
-export default App;
+function Step() {
+  return (
+    <div className="container">
+      <button>-</button>
+      <p>Step: 1</p>
+      <button>+</button>
+    </div>
+  );
+}
+
+function Count() {
+  return (
+    <div className="container">
+      <button>-</button>
+      <p>Count: 1</p>
+      <button>+</button>
+    </div>
+  );
+}
+
+function DateString() {
+  const date = new Date().toDateString();
+  return (
+    <div className="container">
+      <p>{`${date}`}</p>
+    </div>
+  );
+}
