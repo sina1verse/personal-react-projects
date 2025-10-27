55b40bf19618c65f1652bc1f8319ede54c9357b5
sina <cyrus2nd.tg@gmail.com>
2025-10-27 01:21:20 -0400

    Make step and count relational by making 1 general state in parent element that is used by both children

--- 5-date-counter/src/App.js
+++ 5-date-counter/src/App.js
@@ -1,33 +1,35 @@
 import { useState } from "react";
 
 export default function App() {
+  const [step, setStep] = useState(1);
   return (
     <>
-      <Step />
-      <Count />
+      <Step step={step} setStep={setStep} />
+      <Count step={step} />
       <DateString />
     </>
   );
 }
 
-function Step() {
-  const [step, setStep] = useState(1);
+function Step({ step, setStep }) {
   return (
     <div className="container">
-      <button onClick={() => setStep(() => step - 1)}>-</button>
+      <button onClick={() => setStep(() => (step > 0 ? step - 1 : step))}>
+        -
+      </button>
       <p>Step: {step}</p>
       <button onClick={() => setStep(() => step + 1)}>+</button>
     </div>
   );
 }
 
-function Count() {
-  const [count, setCount] = useState(1);
+function Count({ step }) {
+  const [count, setCount] = useState(0);
   return (
     <div className="container">
-      <button onClick={() => setCount(() => count - 1)}>-</button>
+      <button onClick={() => setCount(() => count - step)}>-</button>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(() => count + 1)}>+</button>
+      <button onClick={() => setCount(() => count + step)}>+</button>
     </div>
   );
 }
