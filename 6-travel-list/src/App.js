3a9b03f563d2cfa912318a0eab5d1cd90828bdc0
sina <cyrus2nd.tg@gmail.com>
2025-10-27 21:04:32 -0400

    Add item list dataset and loop through to reneder

--- 6-travel-list/src/App.js
+++ 6-travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -21,8 +27,28 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
