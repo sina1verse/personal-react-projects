b96c374183dea52147db3106031f23c7d548210b
sina <cyrus2nd.tg@gmail.com>
2025-10-27 21:43:57 -0400

    Add a event handler for submision event

--- 6-travel-list/src/App.js
+++ 6-travel-list/src/App.js
@@ -20,8 +20,11 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
