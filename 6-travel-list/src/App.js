3de53217e4bb8e24af484dc86214c4ea5a6326ce
sina <cyrus2nd.tg@gmail.com>
2025-10-28 20:30:38 -0400

    Handle submission event and refresh the form afterwards

--- 6-travel-list/src/App.js
+++ 6-travel-list/src/App.js
@@ -23,15 +23,35 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+    const newItem = {
+      id: initialItems.length + 1,
+      description,
+      quantity,
+      packed: false,
+    };
+    // initialItems.push({
+    //   id: initialItems.length + 1,
+    //   description: description,
+    //   quantity: quantity,
+    //   packed: false,
+    // });
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
